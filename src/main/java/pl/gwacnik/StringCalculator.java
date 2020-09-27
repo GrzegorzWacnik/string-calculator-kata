fd6423f3e6bff10889bf6d5735b8fb7f2ff85c9a
Grzegorz Wacnik <grzegorzwacnik@gmail.com>
2020-09-27 13:27:45 +0200

    Req 1 - Empty input

--- src/main/java/pl/gwacnik/StringCalculator.java
+++ src/main/java/pl/gwacnik/StringCalculator.java
@@ -3,4 +3,9 @@ package pl.gwacnik;
 public class StringCalculator
 {
 
+    public Integer add(String input) {
+        if(input.isEmpty())
+            return 0;
+        return null;
+    }
 }
--- src/test/java/pl/gwacnik/StringCalculatorTest.java
+++ src/test/java/pl/gwacnik/StringCalculatorTest.java
@@ -7,4 +7,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringCalculatorTest
 {
     private final StringCalculator calculator = new StringCalculator();
+
+    @Test
+    public void testReturnZeroIfEmptyStringInput() {
+        final Integer sum = calculator.add("");
+
+        assertThat(sum).isEqualTo(0);
+    }
 }
